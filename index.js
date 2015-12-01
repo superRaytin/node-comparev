30917e6ec1e3a555e8bdc86929c34b79abc58b60
leon.shi <superRaytin@163.com>
2015-12-01 16:26:00 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+/*
+ * comparev 0.1.0
+ * Compare semver version numbers
+ * https://github.com/superRaytin/node-comparev
+ *
+ * Copyright 2015, SuperRaytin
+ * Released under the MIT license.
+ */
+
+
+(function(global) {
+
+  function compare(v1, v2) {
+    // equal
+    if (v1 === v2) return 0;
+
+    var arr1 = v1.split('.');
+    var arr2 = v2.split('.');
+
+    // convert to number
+    arr1 = arr1.map(function(value) {
+      return parseInt(value);
+    });
+
+    arr2 = arr2.map(function(value) {
+      return parseInt(value);
+    });
+
+    var x = arr1[0] - arr2[0];
+    var y = arr1[1] - arr2[1];
+    var z = arr1[2] - arr2[2];
+
+    if (x > 0) {
+      return 1;
+    } else if(x === 0 && y > 0) {
+      return 1;
+    } else if(x === 0 && y === 0 && z > 0) {
+      return 1;
+    }
+
+    return -1;
+  }
+
+  /*
+   * export via AMD, otherwise leak a global
+   * */
+  if (typeof define === 'function' && define.amd) {
+    define(function(){
+      return compare;
+    });
+  } else if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = compare;
+  } else {
+    global.comparev = compare;
+  }
+
+})(this);
---
+++ test/comparev.js
@@ -0,0 +1,27 @@
+
+var should = require('should');
+var comparev = require('../index');
+
+describe('compare', function() {
+  it('should compare() work', function() {
+    comparev('0.8.1', '0.8.1').should.equal(0);
+
+    comparev('0.8.10', '0.8.3').should.equal(1);
+    comparev('0.9.1', '0.8.30').should.equal(1);
+    comparev('0.9.11', '0.8.30').should.equal(1);
+    comparev('0.92.11', '0.10.389').should.equal(1);
+    comparev('1.7.11', '0.8.30').should.equal(1);
+    comparev('1.7.11', '1.3.30').should.equal(1);
+    comparev('1.7.11', '1.7.3').should.equal(1);
+    comparev('1.217.9', '1.7.3').should.equal(1);
+
+    comparev('0.8.1', '0.8.3').should.equal(-1);
+    comparev('0.9.11', '0.10.3').should.equal(-1);
+    comparev('0.92.11', '0.100.3').should.equal(-1);
+    comparev('1.7.11', '1.13.30').should.equal(-1);
+    comparev('1.7.11', '1.7.39').should.equal(-1);
+    comparev('1.7.9', '1.217.3').should.equal(-1);
+
+    comparev.should.be.a.Function;
+  });
+});
