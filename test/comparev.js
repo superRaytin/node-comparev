5f5732bb8a6ab7d6b48c87415b3e9e3f5201e09d
leon.shi <superRaytin@163.com>
2015-12-03 00:43:08 +0800

    add test case

--- test/comparev.js
+++ test/comparev.js
@@ -2,10 +2,8 @@
 var should = require('should');
 var comparev = require('../index');
 
-describe('compare', function() {
-  it('should compare() work', function() {
-    comparev('0.8.1', '0.8.1').should.equal(0);
-
+describe('should compare() work', function() {
+  it('should return 1 when v1 > v2', function() {
     comparev('0.8.10', '0.8.3').should.equal(1);
     comparev('0.9.1', '0.8.30').should.equal(1);
     comparev('0.9.11', '0.8.30').should.equal(1);
@@ -16,7 +14,13 @@ describe('compare', function() {
     comparev('1.217.9', '1.7.3').should.equal(1);
     comparev('0.10.11', '0.9.389').should.equal(1);
     comparev('1.0.0', '0.99.999').should.equal(1);
+  });
 
+  it('should return 0 when v1 = v2', function() {
+    comparev('0.8.1', '0.8.1').should.equal(0);
+  });
+
+  it('should return -1 when v1 < v2', function() {
     comparev('1.7.11', '1.13.30').should.equal(-1);
     comparev('0.8.1', '0.8.3').should.equal(-1);
     comparev('0.9.11', '0.10.3').should.equal(-1);
@@ -24,7 +28,5 @@ describe('compare', function() {
     comparev('1.7.11', '1.13.30').should.equal(-1);
     comparev('1.7.11', '1.7.39').should.equal(-1);
     comparev('1.7.9', '1.217.3').should.equal(-1);
-
-    comparev.should.be.a.Function;
   });
 });
